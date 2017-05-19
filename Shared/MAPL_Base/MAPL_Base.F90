7279e8fbaee30daf066cc945fa584afb09de9343
Lizzie Lundgren <elundgren@seas.harvard.edu>
2017-05-19 12:04:14 -0400

    Create new RESTART attribute for when species are bootstrapped in MAPL
    Changing the internal state RESTART attribute per species from
    MAPL_RestartOptional to MAPL_RestartBootstrap allows distinguishing
    which species were bootstrapped (default values used) versus which
    species were found in the restart file. This enables over-writing the
    bootstrapped species with background concentration values in the species
    database. Ultimately this allows the user to omit species from the
    restart file.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- Shared/MAPL_Base/MAPL_Base.F90
+++ Shared/MAPL_Base/MAPL_Base.F90
@@ -140,6 +140,7 @@ integer, public, parameter :: MAPL_HorzTransOrderSample   = 99
 integer, public, parameter :: MAPL_RestartOptional = 0
 integer, public, parameter :: MAPL_RestartSkip = 1
 integer, public, parameter :: MAPL_RestartRequired = 2
+integer, public, parameter :: MAPL_RestartBootstrap = 3
 
 character(len=ESMF_MAXSTR), public, parameter :: MAPL_StateItemOrderList = 'MAPL_StateItemOrderList'
 character(len=ESMF_MAXSTR), public, parameter :: MAPL_BundleItemOrderList = 'MAPL_BundleItemOrderList'
