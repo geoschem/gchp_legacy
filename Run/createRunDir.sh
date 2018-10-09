637b3965b3ea1239159438b7c6eb2fdeb5a6a0ba
Lizzie Lundgren <elundgren@seas.harvard.edu>
2018-10-09 14:48:41 -0400

    Add features to GCHP run directory creation script
    New features added:
     1. Input data root path is now stored in $HOME/.geoschem/config for
        subsequent usage in creating GCHP run directories. The file is created
        the first time to user specifies the path using creatRunDir.sh.
     2. The CodeDir symbolic link is set to the path of the source code from
        which the run directory is created. Users can still change the link
        using setCodeDir within the run directory.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- Run/createRunDir.sh
+++ Run/createRunDir.sh
@@ -4,37 +4,64 @@
 #
 # Optional argument: run directory name
 #
-# Usage: ./createRunDir.sh [name]
+# If optional run directory name argument is not passed then the user
+# will be prompted to enter a name interactively, or choose to use the
+# default name gchp_{simulation}/
+#
+# Usage: ./createRunDir.sh [rundirname]
 #
 # Initial version: E. Lundgren,10/5/2018
 
 curdir=$(pwd)
-cd ${curdir}/Run
+echo ${curdir}
+cd ../../
+codedir=$(pwd)
+echo ${codedir}
+cd ${curdir}
+echo $(pwd)
 
-#------------------------
-# Prompt user for inputs
-#------------------------
-
-# ExtData path
-default=/n/holylfs/EXTERNAL_REPOS/GEOS-CHEM/gcgrid/data/ExtData
-printf "\nEnter path for ExtData, or press return to accept default:\n"
-printf "(Default = ${default})\n"
-read extdata
-if [[ -z ${extdata} ]]; then
-    extdata=$default
+#-----------------------------------------------------------------
+# Export data root path in ~/.geoschem/config if file exists
+#-----------------------------------------------------------------
+if [[ -f ${HOME}/.geoschem/config ]]; then
+    source ${HOME}/.geoschem/config
+    if [[ ! -d ${GC_DATA_ROOT} ]]; then
+	printf "\nWarning: Default root data directory ${GC_DATA_ROOT} does not exist. Set new path below or edit in ${HOME}/.geoschem/config.\n"
+    fi
+else
+    mkdir -p ${HOME}/.geoschem
 fi
-#if [[ ! -d ${extdata} ]]; then
-#    printf "\nError: ${extdata} does not exist! Exiting.\n"
-#    exit 1
-#fi
 
-# Simulation type
-printf "Choose simulation type:\n"
+#-----------------------------------------------------------------
+# One-time configuration of data root path in ~/.geoschem/config
+#-----------------------------------------------------------------
+if [[ -z "${GC_DATA_ROOT}" ]]; then
+    printf "\nEnter path for ExtData. This will be saved to ${HOME}/.geoschem/config for future automatic use.\n"
+    valid_path=0
+    while [ "$valid_path" -eq 0 ]
+    do
+	read extdata
+	if [[ ${extdata} = "q" ]]; then
+	    printf "\nExiting.\n"
+	    exit 1
+	elif [[ ! -d ${extdata} ]]; then
+            printf "\nError: ${extdata} does not exist. Enter a new path or hit q to quit.\n"
+	else
+	    valid_path=1
+	    echo "export GC_DATA_ROOT=${extdata}" > ${HOME}/.geoschem/config
+	fi
+    done
+fi
+
+#-----------------------------------------------------------------
+# Ask user to select simulation type
+#-----------------------------------------------------------------
+printf "\nChoose simulation type:\n"
 printf "  1. RnPbBe\n"
 printf "  2. Standard\n"
 printf "  3. Benchmark\n"
 valid_sim=0
-while [ "$valid_sim" -eq 0 ]
+while [ "${valid_sim}" -eq 0 ]
 do
     read sim_num
     if [[ ${sim_num} = "1" ]]; then
@@ -57,32 +84,47 @@ do
     fi
 done
 
-# Run directory path
-printf "\nEnter run directory path (excluding run directory name):\n"
-read rundir_path
-if [[ ! -d ${rundir_path} ]]; then
-    printf "\nError: ${rundir_path} does not exist! Exiting.\n"
-    exit 1
-fi
+#-----------------------------------------------------------------
+# Ask user to define path where directoy will be created
+#-----------------------------------------------------------------
+printf "\nEnter path where the run directory will be created:\n"
+valid_path=0
+while [ "$valid_path" -eq 0 ]
+do
+    read rundir_path
+    if [[ ${rundir_path} = "q" ]]; then
+	printf "\nExiting.\n"
+	exit 1
+    elif [[ ! -d ${rundir_path} ]]; then
+        printf "\nError: ${rundir_path} does not exist. Enter a new path or hit q to quit.\n"
+    else
+	valid_path=1
+    fi
+done
  
-#-----------------------------------
-# Create and populate run directory
-#-----------------------------------
-
-# Set run directory path, either gchp_{simulation} or optionally passed name
-if [ ! -z "$1" ]; then
-    rundir_name=$1
+#-----------------------------------------------------------------
+# Ask user to define run directoy name if not passed as argument
+#-----------------------------------------------------------------
+if [ -z "$1" ]; then
+    printf "\nEnter run directory name, or press return to use default:\n"
+    read rundir_name
+    if [[ -z "${rundir_name}" ]]; then
+	rundir_name=gchp_${sim_name}
+	printf "Using default directory name ${rundir_name}\n"
+    fi
 else
-    rundir_name=gchp_${sim_name}
+    rundir_name=$1
 fi
-rundir=${rundir_path}/${rundir_name}
 
-# Make run directory, prompting for a new name if it already exists
+#-----------------------------------------------------------------
+# Ask user for a new run directory name if specified one exists
+#-----------------------------------------------------------------
+rundir=${rundir_path}/${rundir_name}
 valid_rundir=0
 while [ "${valid_rundir}" -eq 0 ]
 do
     if [[ -d ${rundir} ]]; then
-	printf "Warning! ${rundir_name} already exists at that path.\n"
+	printf "Warning! ${rundir} already exists.\n"
         printf "Enter a different run directory name, or q to quit:\n"
 	read new_rundir
 	if [[ ${new_rundir} = "q" ]]; then
@@ -95,9 +137,15 @@ do
         valid_rundir=1
     fi
 done
+
+#-----------------------------------------------------------------
+# Create run directory
+#-----------------------------------------------------------------
 mkdir -p ${rundir}
 
-# Copy generic files for all GCHP run directories
+#-----------------------------------------------------------------
+# Copy run directory files and subdirectories
+#-----------------------------------------------------------------
 cp ./archiveRun.sh         ${rundir} 
 cp ./build.sh              ${rundir} 
 cp ./fvcore_layout.rc      ${rundir} 
@@ -108,38 +156,41 @@ cp ./Makefile              ${rundir}
 cp ./gitignore             ${rundir}/.gitignore
 cp ./GCHP.rc_template      ${rundir}/GCHP.rc
 cp ./runConfig.sh_template ${rundir}/runConfig.sh
-
-# Copy generic subdirectories for all GCHP run directories
-cp -r ./bashrcSamples    ${rundir} 
-cp -r ./OutputDir        ${rundir} 
-cp -r ./runScriptSamples ${rundir}
-
-# Copy simulation-dependent files
-cp ./HISTORY.rc_templates/HISTORY.rc.${sim_name}         ${rundir}/HISTORY.rc
-cp ./input.geos_templates/input.geos.${sim_name}         ${rundir}/input.geos
-cp ./CAP.rc_templates/CAP.rc.${sim_type}                 ${rundir}/CAP.rc
-cp ./ExtData.rc_templates/ExtData.rc.${sim_type}         ${rundir}/ExtData.rc
+cp -r ./bashrcSamples      ${rundir} 
+cp -r ./OutputDir          ${rundir} 
+cp -r ./runScriptSamples   ${rundir}
+cp ./HISTORY.rc_templates/HISTORY.rc.${sim_name}            ${rundir}/HISTORY.rc
+cp ./input.geos_templates/input.geos.${sim_name}            ${rundir}/input.geos
+cp ./CAP.rc_templates/CAP.rc.${sim_type}                    ${rundir}/CAP.rc
+cp ./ExtData.rc_templates/ExtData.rc.${sim_type}            ${rundir}/ExtData.rc
 cp ./HEMCO_Config.rc_templates/HEMCO_Config.rc.${sim_type}  ${rundir}/HEMCO_Config.rc
-cp ./HEMCO_Diagn.rc_templates/HEMCO_Diagn.rc.${sim_type} ${rundir}/HEMCO_Diagn.rc
+cp ./HEMCO_Diagn.rc_templates/HEMCO_Diagn.rc.${sim_type}    ${rundir}/HEMCO_Diagn.rc
 
-## Replace token strings in certain files
-sed -i -e "s|{SIMULATION}|${sim_name_long}|" ${rundir}/GCHP.rc
-sed -i -e "s|{SIMULATION}|${sim_name_long}|" ${rundir}/runConfig.sh
-sed -i -e "s|{DATA_ROOT}|${extdata}|"        ${rundir}/input.geos
-
-# Create symbolic links to data directories
-ln -s ${extdata}/CHEM_INPUTS               ${rundir}/ChemDataDir
-ln -s ${extdata}/HEMCO                     ${rundir}/MainDataDir
-ln -s ${extdata}/GEOS_0.25x0.3125/GEOS_FP  ${rundir}/MetDir
-ln -s ${extdata}/GCHP/TileFiles            ${rundir}/TileFiles
-
-# Create symbolic links to restart files
+#--------------------------------------------------------------------
+# Create symbolic links to data directories, restart files, and code
+#--------------------------------------------------------------------
+ln -s ${codedir}                                ${rundir}/CodeDir
+# NOTE: CodeDir is set to point to GCHP/..; reset using setCodeDir in rundir.
+ln -s ${GC_DATA_ROOT}/CHEM_INPUTS               ${rundir}/ChemDataDir
+ln -s ${GC_DATA_ROOT}/HEMCO                     ${rundir}/MainDataDir
+ln -s ${GC_DATA_ROOT}/GEOS_0.25x0.3125/GEOS_FP  ${rundir}/MetDir
+ln -s ${GC_DATA_ROOT}/GCHP/TileFiles            ${rundir}/TileFiles
+restarts=${GC_DATA_ROOT}/SPC_RESTARTS
 for N in 24 48 90 180 360
 do
     ln -s ${restarts}/initial_GEOSChem_rst.c${N}_${sim_name_long}.nc  ${rundir}
 done
 
+#-----------------------------------------------------------------
+# Replace token strings in certain files
+#-----------------------------------------------------------------
+sed -i -e "s|{SIMULATION}|${sim_name_long}|" ${rundir}/GCHP.rc
+sed -i -e "s|{SIMULATION}|${sim_name_long}|" ${rundir}/runConfig.sh
+sed -i -e "s|{DATA_ROOT}|${GC_DATA_ROOT}|"   ${rundir}/input.geos
+
+#-----------------------------------------------------------------
 # Set permissions
+#-----------------------------------------------------------------
 chmod 744 ${rundir}/setCodeDir
 chmod 744 ${rundir}/build.sh
 chmod 744 ${rundir}/Makefile
@@ -147,21 +198,20 @@ chmod 744 ${rundir}/runConfig.sh
 chmod 744 ${rundir}/archiveRun.sh
 chmod 744 ${rundir}/runScriptSamples/*
 chmod 744 ${rundir}/bashrcSamples/*
+chmod 644 ${rundir}/runScriptSamples/README
 
-#-----------------------------------
-# Save version information
-#-----------------------------------
-
+#----------------------------------------------------------------------
+# Archive GCHP repository version in run directory file rundir.version
+#----------------------------------------------------------------------
 version_log=${rundir}/rundir.version
 echo "This run directory was created by createRunDir.sh from the GCHP repository on $(date)." > ${version_log}
 echo " " >> ${version_log}
 echo "Version information (see also github.com/geoschem/gchp):" >> ${version_log}
 echo $(git log -n 1) >> ${version_log}
 
-#---------------------------------------------------
-# Put run directory under version control (optional)
-#---------------------------------------------------
-
+#-----------------------------------------------------------------
+# Ask user whether to track run directory changes with git
+#-----------------------------------------------------------------
 printf "\nDo you want to track run directory changes with git? (y/n)\n"
 read enable_git
 valid_response=0
@@ -184,7 +234,15 @@ do
     fi
 done
 
-#-------
+#-----------------------------------------------------------------
 # Done!
-#-------
+#-----------------------------------------------------------------
 printf "\nCreated ${rundir}\n"
+printf "\nSource code link CodeDir is set to ${codedir} in the run directory.\n"
+printf "\nPrinting source code repository information...\n\n"
+cd ${rundir}
+make printbuildinfo
+cd ${curdir}
+printf "\nType 'make printbuildinfo' at any time in the run directory to view source c\
+ode status.\n"
+printf "\nUse setCodeDir script to point to a different source code directory.\n"
