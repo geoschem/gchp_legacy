21974d8118d9dc32b0454a0ab702ecdabb6f086b
Liam Bindle <liam.bindle@gmail.com>
2019-03-20 19:28:14 -0300

    updates to FindESMF; moved find_packages to appropriate directories

--- CMakeScripts/GC-GetGCHPThirdParty.cmake
+++ CMakeScripts/GC-GetGCHPThirdParty.cmake
@@ -1,21 +1,9 @@
-# Get third party install path
-set_dynamic_default(THIRD_PARTY "<path to third party install>"
-    LOG THIRD_PARTY_LOG 
-	IS_DIRECTORY
-)
-dump_log(THIRD_PARTY_LOG)
-get_filename_component(THIRD_PARTY "${THIRD_PARTY}" ABSOLUTE)
-
-# Add GCHP's third party libraries
-set(THIRD_PARTY_LIBNAMES
-	-L${THIRD_PARTY}/lib
-	esmf 
-)
-
-# Find NetCDF, MPI, and OpenMP and make them dependees
-find_package(NetCDF REQUIRED COMPONENTS F90)
-find_package(MPI REQUIRED)
+# Find required packages
 find_package(OpenMP REQUIRED)
+find_package(MPI REQUIRED)
+find_package(ESMF REQUIRED)
+find_package(NetCDF REQUIRED COMPONENTS F90)
+
 string(STRIP "${MPI_Fortran_LINK_FLAGS}" MPI_Fortran_LINK_FLAGS)
 target_compile_options(BaseTarget
 	INTERFACE ${MPI_Fortran_COMPILE_FLAGS}
@@ -24,10 +12,9 @@ target_include_directories(BaseTarget
 	INTERFACE ${NETCDF_F90_INCLUDE_DIR} ${MPI_Fortran_INCLUDE_PATH} ${THIRD_PARTY}/include
 )
 target_link_libraries(BaseTarget 
-	PUBLIC 
+	INTERFACE 
 		MAPL_Base MAPL_cfio_r4 GMAO_mpeu GMAO_pilgrim
 		FVdycoreCubed_GridComp fvdycore GFDL_fms GEOS_Shared GMAO_hermes 
-	INTERFACE 
 		${THIRD_PARTY_LIBNAMES} 
 		${NETCDF_LIBRARIES} 
 		${MPI_Fortran_LINK_FLAGS} ${MPI_Fortran_LIBRARIES} 
