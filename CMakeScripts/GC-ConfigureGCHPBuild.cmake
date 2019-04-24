8cfabe2c30f9ab72aa31118bfc0113a33a57672d
Liam Bindle <liam.bindle@gmail.com>
2019-04-24 20:28:41 -0300

    Cleaned up files

--- CMakeScripts/GC-ConfigureGCHPBuild.cmake
+++ CMakeScripts/GC-ConfigureGCHPBuild.cmake
@@ -1,3 +1,61 @@
+#[[ GC-ConfigureGCHPBuild.cmake
+
+This file configures BaseTarget for a GCHP build. This file does
+three things:
+
+    1) Finds dependencies needed by GCHP. Note that CMake scripts in 
+       Shared/ also find_package's.
+
+    2) Sets the appropriate preprocessor definitions for the run directory.
+
+    3) Sets the default compiler flags.
+
+]]
+
+#[[--------------------------------------------------------------------------]]
+#[[     Finding dependencies.                                                ]]
+#[[--------------------------------------------------------------------------]]
+find_package(OpenMP REQUIRED)
+find_package(MPI REQUIRED)
+find_package(ESMF REQUIRED)
+find_package(NetCDF REQUIRED COMPONENTS F90)
+
+# Setup BaseTarget
+string(STRIP "${MPI_Fortran_LINK_FLAGS}" MPI_Fortran_LINK_FLAGS)
+target_compile_options(BaseTarget
+    INTERFACE 
+        ${MPI_Fortran_COMPILE_FLAGS}
+)
+target_include_directories(BaseTarget
+    INTERFACE 
+        ${NETCDF_F90_INCLUDE_DIR} 
+        ${MPI_Fortran_INCLUDE_PATH}
+)
+target_link_libraries(BaseTarget 
+	INTERFACE 
+		MAPL_Base MAPL_cfio_r4 GMAO_mpeu GMAO_pilgrim
+		FVdycoreCubed_GridComp fvdycore GFDL_fms_r4 GEOS_Shared GMAO_hermes 
+		${NETCDF_LIBRARIES} 
+        ${MPI_Fortran_LINK_FLAGS} 
+        ${MPI_Fortran_LIBRARIES} 
+        ${OpenMP_Fortran_FLAGS} 
+        /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+)
+
+# Print repository's last commit
+get_cwd_last_commit_hash(GC_LAST_COMMIT   ${CMAKE_SOURCE_DIR})
+get_cwd_last_commit_hash(GCHP_LAST_COMMIT ${CMAKE_SOURCE_DIR}/GCHP)
+message(STATUS "GEOS-Chem repository: ${GC_LAST_COMMIT}")
+message(STATUS "GCHP repository:      ${GCHP_LAST_COMMIT}")
+
+
+#[[--------------------------------------------------------------------------]]
+#[[     Setting preprocessor definitions.                                    ]]
+#[[--------------------------------------------------------------------------]]
+
+
+#[[     Get defaults for settings by inspecting the run directory.           ]]
+
 # Inspect rundir name to guess the chemistry mechanism
 if("${RUNDIR_NAME}" MATCHES ".*benchmark")
     set(RUNDIR_MECH "benchmark")
@@ -6,6 +64,8 @@ elseif("${RUNDIR_NAME}" MATCHES ".*RnPbBe")
 else()
     set(RUNDIR_MECH "Standard")
 endif()
+
+#[[     Settings TUI with defaults from the run directory inspection.        ]]
 
 # Chemistry mechanism
 set_dynamic_option(MECH "${RUNDIR_MECH}"
@@ -52,10 +112,22 @@ dump_log(EXTRA_DEFS_LOG)
 string(REPLACE " " ";" GC_DEFINES "${GC_DEFINES}")
 set_dynamic_default(GC_DEFINES LOG RESULTING_DEFINES_LOG)
 
+#[[     Set resulting defintions on BaseTarget.                              ]]
+target_compile_definitions(BaseTarget 
+    INTERFACE 
+        ${GC_DEFINES}
+)
+unset(GC_DEFINES)
+
+
+#[[--------------------------------------------------------------------------]]
+#[[     Setting default compiler options.                                    ]]
+#[[--------------------------------------------------------------------------]]
+
 # Get compiler options
 if("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "Intel")
     set_dynamic_default(FC_OPTIONS
-        -fPIC -cpp -w -auto -noalign "-convert big_endian" -O2 -vec-report0 
+        -fPIC -cpp -w -auto -noalign "-convert big_endian" -vec-report0 
         "-fp-model source" -openmp -mcmodel=medium -shared-intel -traceback
         -DLINUX_IFORT
 
@@ -77,12 +149,16 @@ endif()
 message(STATUS "Resulting definitions/options:")
 dump_log(RESULTING_DEFINES_LOG)
 
-target_compile_definitions(BaseTarget INTERFACE ${GC_DEFINES})
-target_compile_options(BaseTarget INTERFACE ${FC_OPTIONS})
-unset(GC_DEFINES)
+target_compile_options(BaseTarget 
+    INTERFACE 
+        ${FC_OPTIONS}
+
+        # Debug flags
+        $<$<AND:$<CXX_COMPILER_ID:Intel>,$<CONFIG:DEBUG>>:-g -O0>
+        $<$<AND:$<CXX_COMPILER_ID:GNU>,  $<CONFIG:DEBUG>>:-g -Og>
+
+        # Release flags
+        $<$<AND:$<CXX_COMPILER_ID:Intel>,$<CONFIG:DEBUG>>:-O2>
+        $<$<AND:$<CXX_COMPILER_ID:GNU>,  $<CONFIG:DEBUG>>:-O3>
+)
 unset(FC_OPTIONS)
-
-# Include the third party libraries
-include(GC-GetGCHPThirdParty)
-
-
