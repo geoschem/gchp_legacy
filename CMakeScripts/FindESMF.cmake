6e0b7f16eb8bbc65986ac40c14962ebc5eb74eb3
Liam Bindle <liam.bindle@gmail.com>
2019-05-17 13:04:34 -0300

    updated FindESMF and FindGFTL to use CMAKE_PREFIX_PATH

--- CMakeScripts/FindESMF.cmake
+++ CMakeScripts/FindESMF.cmake
@@ -1,132 +1,68 @@
 
-#[[
-find_path -- used to find the path to a directory containing the given file
-
-find_path(VAR filename)
-
-Creates a cache entry for VAR. If VAR is set, find_path will not run. If 
-the file isn't found the the value will be VAR-NOT_FOUND.
-
-HINTS is for dynamically generated guesses
-PATHS is for hard-coded guesses
-]]
-
 
 find_path(ESMF_INCLUDES_DIR
-	ESMF_ErrReturnCodes.inc 
-	HINTS 
-		${ESMF_INSTALL} 
-		${CMAKE_BINARY_DIR}/${ESMF_INSTALL}
-		$ENV{ESMF_INSTALL_PREFIX} 
-		$ENV{ESMF_INSTALL_HEADERDIR}
-		$ENV{ESMF_DIR}/DEFAULTINSTALLDIR
-		$ENV{ESMF_DIR}/src
+	ESMF_ErrReturnCodes.inc
 	DOC "The path to the directory containing \"ESMF_ErrReturnCodes.inc\"."
 	PATH_SUFFIXES
 		"include" 
 		"mod"
 )
+
 find_path(ESMF_HEADERS_DIR
 	ESMC.h
-	HINTS 
-		${ESMF_INSTALL} 
-		${CMAKE_BINARY_DIR}/${ESMF_INSTALL}
-		$ENV{ESMF_INSTALL_PREFIX} 
-		$ENV{ESMF_INSTALL_HEADERDIR}
-		$ENV{ESMF_DIR}/DEFAULTINSTALLDIR
 	DOC "The path to the directory containing \"ESMC.h\"."
 	PATH_SUFFIXES "include"
 )
 
-
-file(GLOB GLOB_MATCHES
-	${ESMF_INSTALL}/mod/mod*/*.*.*.*.*/esmf.mod
-	${CMAKE_BINARY_DIR}/${ESMF_INSTALL}/mod/mod*/*.*.*.*.*/esmf.mod
-	$ENV{ESMF_INSTALL_PREFIX}/mod/mod*/*.*.*.*.*/esmf.mod
-	$ENV{ESMF_DIR}/DEFAULTINSTALLDIR/mod/mod*/*.*.*.*.*/esmf.mod
+file_glob_directories(GLOBBED_MODDIRS
+	PATTERNS
+		mod/mod*/*.*.*.*.*/esmf.mod
+	PATHS
+		${CMAKE_PREFIX_PATH}
 )
-set(ESMF_MODDIRS_GLOBBED "")
-foreach(GLOBBED_FILE ${GLOB_MATCHES})
-	get_filename_component(GLOBBED_DIR ${GLOBBED_FILE} DIRECTORY)
-	list(APPEND ESMF_MODDIRS_GLOBBED ${GLOBBED_DIR})
-endforeach()
-list(REMOVE_DUPLICATES  ESMF_MODDIRS_GLOBBED)
 find_path(ESMF_MOD_DIR
 	esmf.mod
 	HINTS
-		${ESMF_MODDIRS_GLOBBED}
-		${ESMF_INSTALL}
-		${CMAKE_BINARY_DIR}/${ESMF_INSTALL}
-		$ENV{ESMF_INSTALL_PREFIX}
-		$ENV{ESMF_INSTALL_MODDIR}
+		${GLOBBED_MODDIRS}
 	DOC "The path to the directory containing \"esmf.mod\"."
 	PATH_SUFFIXES 
 		"mod"
 		"include"
 )
 
-
-file(GLOB GLOB_MATCHES
-	${ESMF_INSTALL}/lib/lib*/*.*.*.*.*/libesmf.a
-	${CMAKE_BINARY_DIR}/${ESMF_INSTALL}/lib/lib*/*.*.*.*.*/libesmf.a
-	$ENV{ESMF_INSTALL_PREFIX}/lib/lib*/*.*.*.*.*/libesmf.a
-	$ENV{ESMF_DIR}/DEFAULTINSTALLDIR/lib/lib*/*.*.*.*.*/libesmf.a
+file_glob_directories(GLOBBED_MODDIRS
+	PATTERNS
+		lib/lib*/*.*.*.*.*/libesmf.a
+	PATHS
+		${CMAKE_PREFIX_PATH}
 )
-set(ESMF_LIBDIRS_GLOBBED "")
-foreach(GLOBBED_FILE ${GLOB_MATCHES})
-	get_filename_component(GLOBBED_DIR ${GLOBBED_FILE} DIRECTORY)
-	list(APPEND ESMF_LIBDIRS_GLOBBED ${GLOBBED_DIR})
-endforeach()
-list(REMOVE_DUPLICATES  ESMF_LIBDIRS_GLOBBED)
 find_library(ESMF_LIBRARIES
-	esmf
+	libesmf.a
 	HINTS
-		${ESMF_LIBDIRS_GLOBBED}
-		${ESMF_INSTALL}
-		${CMAKE_BINARY_DIR}/${ESMF_INSTALL}
-		$ENV{ESMF_INSTALL_PREFIX}
-		$ENV{ESMF_INSTALL_LIBDIR}
+		${GLOBBED_MODDIRS}
 	DOC "The path to the directory containing \"libesmf.a\"."
 	PATH_SUFFIXES
 		"lib"
 )
 
-
-# Make a readable error message
-set(ESMF_ERRMSG "\nFailed to find some files in your ESMF install that are needed. \
-To resolve this you have two options:\n
-  1) Set ESMF_INSTALL to the directory where ESMF was installed.")
-
-if(IS_ABSOLUTE "${ESMF_INSTALL}" AND EXISTS "${ESMF_INSTALL}")
-    set(ESMF_ERRMSG "${ESMF_ERRMSG}
-	+ ESMF_INSTALL: ${ESMF_INSTALL}")
-elseif(EXISTS "${CMAKE_BINARY_DIR}/${ESMF_INSTALL}")
-    set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_INSTALL: ${CMAKE_BINARY_DIR}/${ESMF_INSTALL}")
-else()
-    set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_INSTALL: Path to where ESMF was installed")
-endif()
-
-set(ESMF_ERRMSG "${ESMF_ERRMSG}\n
-  2) Manually set the following paths which are missing:")
+set(ESMF_ERRMSG "\nCouldn't find the following directories/files:")
 if(NOT ESMF_INCLUDES_DIR)
 	set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_INCLUDES_DIR: Path to directory with ESMF's \".inc\" files")
+    ESMF_INCLUDES_DIR: Directory with ESMF's \".inc\" files (e.g. \"ESMF_ErrReturnCodes.inc\")")
 endif()
 if(NOT ESMF_HEADERS_DIR)
 	set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_HEADERS_DIR:  Path to directory with ESMF's \".h\" files")
+    ESMF_HEADERS_DIR:  Directory with ESMF's \".h\" files   (e.g. \"ESMC.h\")")
 endif()
 if(NOT ESMF_MOD_DIR)
 	set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_MOD_DIR:      Path to directory with ESMF's \".mod\" files")
+    ESMF_MOD_DIR:      Directory with ESMF's \".mod\" files (e.g. \"esmf.mod\")")
 endif()
 if(NOT ESMF_LIBRARIES)
 	set(ESMF_ERRMSG "${ESMF_ERRMSG}
-    + ESMF_LIBRARIES:    Path to \"libesmf.a\"")
+    ESMF_LIBRARIES:    Path to \"libesmf.a\"")
 endif()
-set(ESMF_ERRMSG "${ESMF_ERRMSG}\n\n")
+set(ESMF_ERRMSG "${ESMF_ERRMSG}\n\nLocate the directories/files mentioned above and then set CMAKE_PREFIX_PATH to a semicolon-separated list of directories which will be searched.\n\n")
 
 
 find_package_handle_standard_args(ESMF 
@@ -137,5 +73,3 @@ find_package_handle_standard_args(ESMF
 		ESMF_LIBRARIES
 	FAIL_MESSAGE "${ESMF_ERRMSG}"
 )
-
-mark_as_advanced(ESMF_INSTALL)
--- CMakeScripts/FindGFTL.cmake
+++ CMakeScripts/FindGFTL.cmake
@@ -1,35 +1,15 @@
 
 find_path(GFTL_INCLUDE_DIR
 	types/key_deferredLengthString.inc
-	HINTS 
-		${GFTL_INSTALL}
-		${CMAKE_BINARY_DIR}/${GFTL_INSTALL}
 	PATH_SUFFIXES "include"
 )
 
-# Make a readable error message
-set(GFTL_ERRMSG "\nFailed to find some file in your gFTL install that are needed. \
-To resolve this you have two options:\n
-  1) Set GFTL_INSTALL to the directory where gFTL was installed.")
-
-if(IS_ABSOLUTE "${GFTL_INSTALL}" AND EXISTS "${GFTL_INSTALL}")
-    set(GFTL_ERRMSG "${GFTL_ERRMSG}
-	+ GFTL_INSTALL: ${GFTL_INSTALL}")
-elseif(EXISTS "${CMAKE_BINARY_DIR}/${GFTL_INSTALL}")
-    set(GFTL_ERRMSG "${GFTL_ERRMSG}
-    + GFTL_INSTALL: ${CMAKE_BINARY_DIR}/${GFTL_INSTALL}")
-else()
-    set(GFTL_ERRMSG "${GFTL_ERRMSG}
-    + GFTL_INSTALL: Path to where GFTL was installed")
-endif()
-
-set(GFTL_ERRMSG "${GFTL_ERRMSG}\n
-  2) Manually set the following paths which are missing:")
+set(GFTL_ERRMSG "\nCouldn't find the following directories/files:")
 if(NOT GFTL_INCLUDE_DIR)
 	set(GFTL_ERRMSG "${GFTL_ERRMSG}
-    + GFTL_INCLUDE_DIR: Path to gFTL's \"include/\" directory")
+    GFTL_INCLUDE_DIR: Path to gFTL's \"include/\" directory")
 endif()
-set(GFTL_ERRMSG "${GFTL_ERRMSG}\n\n")
+set(GFTL_ERRMSG "${GFTL_ERRMSG}\n\nLocate the directories/files mentioned above and then set CMAKE_PREFIX_PATH to a semicolon-separated list of directories which will be searched.\n\n")
 
 find_package_handle_standard_args(GFTL
 	REQUIRED_VARS GFTL_INCLUDE_DIR
