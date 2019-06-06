08d12227e4ad67774c8af12fd68df83bece7ffb4
Liam Bindle <liam.bindle@gmail.com>
2019-06-06 09:48:38 -0300

    changed '#if defined( xxxx )' to '#ifdef xxxx' in Chem_GridCompMod.F90; see #28 on GCHP

--- Chem_GridCompMod.F90
+++ Chem_GridCompMod.F90
@@ -41,7 +41,7 @@
 !\\
 ! !INTERFACE:
 !
-#if defined( MODEL_GEOS )
+#ifdef MODEL_GEOS
 MODULE GEOSCHEMchem_GridCompMod
 #else
 MODULE Chem_GridCompMod
@@ -7601,7 +7601,7 @@ CONTAINS
 !EOC
 #endif
 
-#if defined( MODEL_GEOS )
+#ifdef MODEL_GEOS
  END MODULE GEOSCHEMchem_GridCompMod
 #else
  END MODULE Chem_GridCompMod
